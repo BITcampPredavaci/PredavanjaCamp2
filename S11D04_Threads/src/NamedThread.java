07b91d9646e023b7ec25cb820977d4b9bb194288
Benjamin Talic <benjamin.talic@bitcamp.ba>
2015-07-30 22:35:22 +0200

    threads

---
+++ S11D04_Threads/src/NamedThread.java
@@ -0,0 +1,18 @@
+
+public class NamedThread extends Thread {
+	
+	private String name;
+	
+	public NamedThread(String name){
+		this.name = name;
+	}
+	
+	@Override
+	public void run(){
+		for(int i = 0; i < 10; i++){
+			System.out.println(name + " counted to: " + i);
+		}
+	}
+	
+
+}
---
+++ S11D04_Threads/src/PrimeCounter.java
@@ -0,0 +1,43 @@
+
+public class PrimeCounter extends Thread{
+	private static Object o = new Object();
+	private int start;
+	private int end;
+	private static int count;
+	
+	public PrimeCounter(int start, int end){
+		this.start = start;
+		this.end = end;
+	}
+	
+	public void countPrimes(){
+		for(int i = start; i < end; i++){
+			if(isPrime(i)){
+				synchronized(o){
+					count++;
+				}
+			}
+		}
+	}
+	
+	private synchronized void incCount(){
+		count++;
+	}
+	
+	private boolean isPrime(int number){
+		for(int i = 2; i < number/2; i++)
+			if(number % i == 0)
+				return false;
+		return true;
+	}
+	
+	public int getCount(){
+		return count;
+	}
+	
+	@Override
+	public void run(){
+		countPrimes();
+	}
+
+}
